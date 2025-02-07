bda473e3061803a3804ff8171d18c08a97ebb3ae
Kamil <example@example.com>
2025-02-07 16:35:28 +0100

    listing, product cart, prisma connection

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+import ProductList from "@/components/shared/product/product-list";
+import { getLatestProducts } from "@/lib/actions/product.actions";
+
+const HomePage = async () => {
+  const latestProducts = await getLatestProducts();
+
+  return (
+    <div className="space-y-8">
+      <ProductList title="Newest Arrivals" data={latestProducts} />
+    </div>
+  );
+};
+
+export default HomePage;
